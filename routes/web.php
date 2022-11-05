f3212e9890c51c2395978b59f66675a05ec83985
unknown <arbaz.khanmansoor@nectardigit.com>
2022-11-05 20:38:08 +0545

    error-fixed

--- routes/web.php
+++ routes/web.php
@@ -26,15 +26,15 @@ Route::get('/admin', function(){
 
 
 
-Route::group(['prefix'=>'admin'], function(){
+// Route::group(['prefix'=>'admin'], function(){
 
-// sidebar route
+// // sidebar route
 Route::get('/manage-slider', [SliderController::class, 'index'])->name('manage-slider');
 Route::get('/create-slider', [SliderController::class, 'view'])->name('create-slider');
 Route::get('/create-gallery', [AlbumController::class, 'index'])->name('create-gallery');
 Route::get('/manage-gallery', [AlbumController::class, 'view'])->name('manage-gallery');
 
-// Slider
+// // Slider
 Route::post('/createSlider', [SliderController::class, 'store'])->name('createSlider.index');
 Route::delete('/manage-slider/{id}', [SliderController::class, 'delete'])->name('delete.slider');
 Route::get('/createSlider/{id}',[SliderController::class, 'edit'])->name('slider.edit');
@@ -45,9 +45,8 @@ Route::post('createAblum', [AlbumController::class, 'store'])->name('album.creat
 Route::delete('albumdelete/{id}', [AlbumController::class, 'delete'])->name('album.delete');
 Route::get('albumedit/{id}', [AlbumController::class, 'edit'])->name('album.edit');
 
-// Testimonial
-Route::ge('/manage-testimonial',[])->name('manage-testimonial');
-
-});
+// // Testimonial
+// Route::ge('/manage-testimonial',[])->name('manage-testimonial');
+// });
 
 
